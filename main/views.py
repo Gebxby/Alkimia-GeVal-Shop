dd6f41fa067970114f43812119f2cdcd3c5b64b5
Gebxby <gabrielfenanilmupengetahuan@gmail.com>
2024-09-10 13:00:48 +0700

    add models

---
+++ main/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def show_main(request):
+    context = {
+        'name' : 'gigi vibranium',
+        'price': '2.000.000.000.000.00',
+        'description': 'barang ini berguna untuk keseharian,mampu membantu nengunyah apapun'
+    }
+
+    return render(request, "main.html", context)
