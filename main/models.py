68b5845d956c1a55b420aad31c9024276cf59949
Gebxby <gabrielfenanilmupengetahuan@gmail.com>
2024-09-22 12:05:39 +0700

    nambahin form register,login,dan logout

--- main/models.py
+++ main/models.py
@@ -1,11 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Menutoko(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     price = models.IntegerField()
     description = models.TextField()
 
-    @property
-    def is_mood_strong(self):
-        return self.mood_intensity > 5
